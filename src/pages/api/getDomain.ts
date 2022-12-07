f70bf0dd95e0ff7f7e53ce60efd220120a865f8f
JiroRauwers <jairorauwers@gmail.com>
2022-12-07 16:06:01 -0300

    fix: api domain

--- src/pages/api/getDomain.ts
+++ src/pages/api/getDomain.ts
@@ -4,7 +4,9 @@ const handler = (
     req: NextApiRequest, res: NextApiResponse
 ) => {
     res.status(200).json({
-        "localhost": "https://s3.sa-east-1.amazonaws.com/template.centralcorretor.com.br/content/bf18d5f9-1fb7-11ed-8aec-0259ada850d2/35fcae87-69a4-11ed-8aec-0259ada850d2/content.json"
+        "localhost": "https://s3.sa-east-1.amazonaws.com/template.centralcorretor.com.br/content/bf18d5f9-1fb7-11ed-8aec-0259ada850d2/35fcae87-69a4-11ed-8aec-0259ada850d2/content.json",
+        "centraltr": "https://s3.sa-east-1.amazonaws.com/template.centralcorretor.com.br/content/bf18d5f9-1fb7-11ed-8aec-0259ada850d2/35fcae87-69a4-11ed-8aec-0259ada850d2/content.json",
+        "centraltr.ee": "https://s3.sa-east-1.amazonaws.com/template.centralcorretor.com.br/content/bf18d5f9-1fb7-11ed-8aec-0259ada850d2/35fcae87-69a4-11ed-8aec-0259ada850d2/content.json"
     })
 }
 
